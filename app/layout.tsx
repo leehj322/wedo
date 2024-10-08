790f40caf75de44548a7a2f48b23c12b4bd33c60
leehj322 <gudwns8956@naver.com>
2024-10-08 23:48:55 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,6 @@ export default function RootLayout({
       <body className={`${pretendard.variable} font-pretendard`}>
         <Providers>{children}</Providers>
       </body>
-      <label>라벨</label>
-      <input type="button" />
     </html>
   );
 }
