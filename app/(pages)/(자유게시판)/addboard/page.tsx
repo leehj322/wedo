f02657513205c8b9b2193c81143714828b5dc48a
gjrefa9139 <gjrefa9139@gmail.com>
2024-10-15 20:39:37 +0900

    Feat: 페이지 추가 (자유게시판) [boards, board, addboard]

---
+++ app/(pages)/(자유게시판)/addboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Addboard() {
+  return (
+    <main className="flex h-full w-full flex-col items-center justify-center">
+      <div>-1</div>
+      <div>0</div>
+      <div>1</div>
+    </main>
+  );
+}
