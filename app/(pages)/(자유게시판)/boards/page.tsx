2994857968c945a1a7579f02af56d99e3714a76a
gjrefa9139 <gjrefa9139@gmail.com>
2024-10-15 21:24:44 +0900

    Test: 작업용 페이지 링크 추가/제거
    렌딩 -> 자유게시판 링크 제거
    자유게시판 -> 게시물 상세, 게시물 등록 링크 추가

--- app/(pages)/(자유게시판)/boards/page.tsx
+++ app/(pages)/(자유게시판)/boards/page.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
+
 export default function Boards() {
   return (
     <main className="flex h-full w-full flex-col items-center justify-center">
       <div>-1</div>
       <div>0</div>
       <div>1</div>
+      <Link className="text-blue-400" href="/board/0">
+        게시물 상세 링크
+      </Link>
+      <Link className="text-blue-400" href="/addboard">
+        게시물 등록 링크
+      </Link>
     </main>
   );
 }
