9eeb0dfb2b4b5e2595bfd0d023f13c19436ee5a5
leehj322 <gudwns8956@naver.com>
2024-10-17 19:26:08 +0900

    feat: 팀 페이지 팀 여부 확인 로직 추가 #46

--- app/(pages)/(team)/[teamid]/page.tsx
+++ app/(pages)/(team)/[teamid]/page.tsx
@@ -1,3 +1,12 @@
-export default function TeamPage() {
-  return <div className="mt-[100px]">팀페이지</div>;
+import { getUser } from "@/apis/user";
+
+export default async function TeamPage() {
+  const res = await getUser();
+  const hasTeam = res.memberships.length !== 0;
+
+  return hasTeam ? (
+    <div className="pt-[84px]">팀있음</div>
+  ) : (
+    <div className="pt-[84px]">팀없음</div>
+  );
 }
