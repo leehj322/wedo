d70d14476d5b8ba806ace5fb361ab95dffb613aa
kjh9852 <kjh985264@gmail.com>
2024-10-17 17:03:27 +0900

    feat: background 색상을 받아 올 수 있도록 props 추가 #55

--- components/@common/container/Container.tsx
+++ components/@common/container/Container.tsx
@@ -2,12 +2,23 @@ import { ReactNode } from "react";
 
 interface ContainerProps {
   children: ReactNode;
+  background: "white" | "lightBeige";
 }
 
-export default function Container({ children }: ContainerProps) {
+const backgroundColors = {
+  white: "inverse",
+  lightBeige: "primary-light",
+};
+
+export default function Container({ children, background }: ContainerProps) {
+  const backgroundClass =
+    backgroundColors[background] || backgroundColors.white;
+
   return (
-    <section className="mx-auto w-full max-w-[1200px]">
-      <div className="px-4 tab:px-6 pc:px-0">{children}</div>
-    </section>
+    <div className={`min-h-[100vh] bg-${backgroundClass}`}>
+      <section className="mx-auto w-full max-w-[1200px] pt-[60px]">
+        <div className="px-4 tab:px-6 pc:px-0">{children}</div>
+      </section>
+    </div>
   );
 }
