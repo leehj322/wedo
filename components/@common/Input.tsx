43bcae631053d5bc5c9b906ba3182ff2cb18688c
gjrefa9139 <gjrefa9139@gmail.com>
2024-10-17 17:47:23 +0900

    Style: 공용 컴포넌트 Input, Textarea Props 앞글자 대문자로 변경
    인텔리센스 사용시 cn 관련 Prop이 먼저 나오도록 수정

--- components/@common/Input.tsx
+++ components/@common/Input.tsx
@@ -13,62 +13,62 @@ const variant = cva(
   ],
   {
     variants: {
-      boxSize: {
+      BoxSize: {
         default: "h-11 tab:h-12",
         sm: "h-11",
         md: "h-12",
         lg: "h-12 tab:h-14",
       },
-      fontSize: {
+      FontSize: {
         default: "md-normal tab:lg-normal",
         sm: "md-normal",
         lg: "lg-normal",
       },
-      padding: {
+      Padding: {
         default: "py-[10px] tab:py-3",
         xs: "py-[6px] tab:py-2",
         sm: "px-3 py-[10px]",
         md: "max-tab:py-3",
         lg: "tab:px-6",
       },
-      bgColor: {
+      BgColor: {
         default: "bg-input-default",
         white: "bg-white",
       },
-      error: {
+      Error: {
         true: "border-danger",
       },
     },
     compoundVariants: [
       {
-        boxSize: "sm",
-        fontSize: "sm",
-        padding: "sm",
+        BoxSize: "sm",
+        FontSize: "sm",
+        Padding: "sm",
         className: "font-medium",
       },
     ],
     defaultVariants: {
-      boxSize: "default",
-      fontSize: "default",
-      padding: "default",
-      bgColor: "default",
+      BoxSize: "default",
+      FontSize: "default",
+      Padding: "default",
+      BgColor: "default",
     },
   },
 );
 
 type Props = InputHTMLAttributes<HTMLInputElement> &
   VariantProps<typeof variant> & {
-    errorMessage?: string;
+    ErrorMessage?: string;
   };
 
 export default forwardRef<HTMLInputElement, Props>(function Input(
   {
-    boxSize,
-    fontSize,
-    padding,
-    bgColor,
-    error,
-    errorMessage,
+    BoxSize,
+    FontSize,
+    Padding,
+    BgColor,
+    Error,
+    ErrorMessage,
     className,
     ...props
   },
@@ -78,12 +78,12 @@ export default forwardRef<HTMLInputElement, Props>(function Input(
     <section className="flex w-full flex-col gap-y-2">
       <input
         className={cn(
-          variant({ boxSize, fontSize, padding, bgColor, error, className }),
+          variant({ BoxSize, FontSize, Padding, BgColor, Error, className }),
         )}
         ref={ref}
         {...props}
       />
-      {errorMessage && <p className="md-medium text-danger">{errorMessage}</p>}
+      {ErrorMessage && <p className="md-medium text-danger">{ErrorMessage}</p>}
     </section>
   );
 });
