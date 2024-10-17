edc96bb5da39f3ddcb5eb32999159045ee44c08f
kjh9852 <kjh985264@gmail.com>
2024-10-18 01:28:40 +0900

    refactor: 조건부로 클래스명 지정 #58

--- components/@common/container/Container.tsx
+++ components/@common/container/Container.tsx
@@ -5,16 +5,12 @@ interface ContainerProps {
   background: "white" | "lightBeige";
 }
 
-const backgroundColors = {
-  white: "inverse",
-  lightBeige: "primary-light",
-};
-
 export default function Container({ children, background }: ContainerProps) {
-  const backgroundClass = backgroundColors[background];
+  const backgroundClass =
+    background === "white" ? "bg-inverse" : "bg-primary-light";
 
   return (
-    <div className={`min-h-[100vh] bg-${backgroundClass}`}>
+    <div className={`min-h-[100vh] ${backgroundClass}`}>
       <section className="mx-auto w-full max-w-[1200px] pt-[60px]">
         <div className="px-4 tab:px-6 pc:px-0">{children}</div>
       </section>
