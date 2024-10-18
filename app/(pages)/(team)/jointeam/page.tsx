d3544f9a55b5a92f1faa4f93753951676e176983
leehj322 <gudwns8956@naver.com>
2024-10-18 14:24:18 +0900

    feat: 팀 참여하기 페이지 UI 레이아웃 구현 #57
    - 기능을 제외한 구현
    - /jointeam 페이지

--- app/(pages)/(team)/jointeam/page.tsx
+++ app/(pages)/(team)/jointeam/page.tsx
@@ -1,3 +1,23 @@
+import { Button } from "@/components/@common/Button";
+import Input from "@/components/@common/Input";
+import Container from "@/components/@common/container/Container";
+
 export default function JoinTeamPage() {
-  return <div className="mt-[100px]">팀 참여 페이지</div>;
+  return (
+    <Container background="lightBeige">
+      <div className="mx-auto pt-[72px] tab:w-[460px] tab:pt-[100px] pc:pt-[140px]">
+        <h1 className="2xl-medium mb-6 text-center text-default-light pc:4xl-medium tab:mb-20">
+          팀 참여하기
+        </h1>
+        <label htmlFor="token">
+          <h3 className="lg-medium mb-3 text-default-light">팀 링크</h3>
+        </label>
+        <Input id="token" placeholder="팀 토큰을 입력해주세요." />
+        <Button className="mb-6 mt-10 w-full">참여하기</Button>
+        <p className="lg-normal text-center text-default-light">
+          공유받은 팀 토큰을 입력해 참여할 수 있어요.
+        </p>
+      </div>
+    </Container>
+  );
 }
