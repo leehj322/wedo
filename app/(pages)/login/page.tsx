3170dfa41b8a6bc2274b40f294a3248a7ab08a83
hvrain <hvrain3222@naver.com>
2024-10-21 16:15:34 +0900

    Feat: 로그인 페이지 구현 light 구현 완료

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,6 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
 
-import { actionSignIn } from "@/apis/action";
+import Container from "@/@common/container/Container";
+import LoginForm from "@/components/auth/login/LoginForm";
+import GoogleIcon from "@/public/images/google.png";
+import KakaoIcon from "@/public/images/kakao.png";
+import { Separator } from "@/ui/separator";
 
 export const GOOGLE_OAUTH_PARAMS = {
   client_id: process.env.GOOGLE_CLIENT_ID!,
@@ -21,26 +26,42 @@ export default function LoginPage() {
   const kakao = new URLSearchParams(KAKAO_OAUTH_PARAMS);
 
   return (
-    <div>
-      <form action={actionSignIn}>
-        <label htmlFor="email">이메일</label>
-        <input id="email" name="email" type="email" defaultValue="" />
-        <label htmlFor="password">비밀번호</label>
-        <input id="password" name="password" type="text" defaultValue="" />
-        <button type="submit">로그인</button>
-      </form>
-      <Link
-        className="w-30 h-10 bg-gray-500"
-        href={`https://accounts.google.com/o/oauth2/v2/auth?${google}`}
-      >
-        구글 로그인
-      </Link>
-      <Link
-        className="w-30 h-10 bg-gray-500"
-        href={`https://kauth.kakao.com/oauth/authorize?${kakao}`}
-      >
-        카카오 로그인
-      </Link>
-    </div>
+    <Container background="lightBeige">
+      <div className="mx-auto mt-6 flex max-w-[460px] flex-col items-center justify-center pb-20 tab:mt-[100px] pc:mt-[140px]">
+        <h1 className="2xl-medium pc:4xl-medium">로그인</h1>
+        <LoginForm />
+        <div className="relative mt-6 h-full w-full tab:mt-11">
+          <Separator className="my-2 bg-brand-border" />
+          <div className="lg-medium absolute left-1/2 right-1/2 top-0 z-10 flex w-[60px] -translate-x-1/2 items-center justify-center bg-primary-light">
+            OR
+          </div>
+        </div>
+        <div className="mt-4 flex w-full items-center gap-4">
+          <span className="lg-medium grow">간편 회원가입하기</span>
+          <Link
+            className="flex items-center justify-center rounded-full border bg-white"
+            href={`https://accounts.google.com/o/oauth2/v2/auth?${google}`}
+          >
+            <Image
+              width={42}
+              height={42}
+              src={GoogleIcon}
+              alt="login to google"
+            />
+          </Link>
+          <Link
+            className="flex items-center justify-center rounded-full border bg-yellow-300"
+            href={`https://kauth.kakao.com/oauth/authorize?${kakao}`}
+          >
+            <Image
+              width={42}
+              height={42}
+              src={KakaoIcon}
+              alt="login to kakao"
+            />
+          </Link>
+        </div>
+      </div>
+    </Container>
   );
 }
---
+++ components/auth/login/LoginForm.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import { FormProvider, useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import z from "zod";
+
+import { Button } from "@/@common/Button";
+import { actionSignIn } from "@/apis/action";
+import FormProviderField from "@/components/auth/InputField";
+
+const INITIAL_LOGIN_STATE = {
+  status: "NOT_YET",
+};
+
+export const loginSchema = z.object({
+  email: z.string().email({ message: "이메일 형식이 올바르지 않습니다." }),
+  password: z
+    .string()
+    .min(8, { message: "비밀번호를 8글자 이상 입력해주세요" })
+    .regex(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/, {
+      message: "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.",
+    }),
+});
+
+export default function LoginForm() {
+  const router = useRouter();
+  const [state, formAction] = useFormState(actionSignIn, INITIAL_LOGIN_STATE);
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onBlur",
+  });
+  if (state.status === "SUCCESS") router.push("/");
+
+  return (
+    <FormProvider {...form}>
+      <form action={formAction} className="flex w-full flex-col">
+        <div className="mt-6 flex flex-col gap-6 tab:mt-[52px] pc:mt-20">
+          <FormProviderField
+            label="이메일"
+            name="email"
+            type="email"
+            placeholder="이메일을 입력해주세요"
+            control={form.control}
+          />
+          <FormProviderField
+            label="비밀번호"
+            name="password"
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+            hasVisibleTrigger
+            control={form.control}
+          />
+        </div>
+        <Link
+          href="/"
+          className="mt-2 self-end text-link-light underline underline-offset-1"
+        >
+          비밀번호를 잊으셨나요?
+        </Link>
+        <Button
+          className="mt-10"
+          disabled={!form.formState.isValid}
+          type="submit"
+        >
+          로그인
+        </Button>
+      </form>
+      <div className="mt-4 space-x-2">
+        <span>아직 계정이 없으신가요?</span>
+        <Link
+          href="/signup"
+          className="self-end text-link-light underline underline-offset-1"
+        >
+          가입하기
+        </Link>
+      </div>
+    </FormProvider>
+  );
+}
