35e2d80319f52cf4ccf51b08a8137b77c196640b
hvrain <hvrain3222@naver.com>
2024-10-21 18:52:21 +0900

    Fix: 소셜 로그인 버튼 수정

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-
 import Container from "@/@common/container/Container";
 import LoginForm from "@/components/auth/login/LoginForm";
+import OAuthLink from "@/components/auth/login/OAuthLink";
 import GoogleIcon from "@/public/images/google.png";
 import KakaoIcon from "@/public/images/kakao.png";
-import { Separator } from "@/ui/separator";
 
 export const GOOGLE_OAUTH_PARAMS = {
   client_id: process.env.GOOGLE_CLIENT_ID!,
@@ -30,36 +27,21 @@ export default function LoginPage() {
       <div className="mx-auto mt-6 flex max-w-[460px] flex-col items-center justify-center pb-20 tab:mt-[100px] pc:mt-[140px]">
         <h1 className="2xl-medium pc:4xl-medium">로그인</h1>
         <LoginForm />
-        <div className="relative mt-6 h-full w-full tab:mt-11">
-          <Separator className="my-2 bg-brand-border" />
-          <div className="lg-medium absolute left-1/2 right-1/2 top-0 z-10 flex w-[60px] -translate-x-1/2 items-center justify-center bg-primary-light">
-            OR
-          </div>
-        </div>
-        <div className="mt-4 flex w-full items-center gap-4">
-          <span className="lg-medium grow">간편 회원가입하기</span>
-          <Link
-            className="flex items-center justify-center rounded-full border bg-white"
+        <div className="mt-[60px] flex w-full flex-col items-center gap-4">
+          <OAuthLink
+            alt="구글 로그인"
+            icon={GoogleIcon}
             href={`https://accounts.google.com/o/oauth2/v2/auth?${google}`}
           >
-            <Image
-              width={42}
-              height={42}
-              src={GoogleIcon}
-              alt="login to google"
-            />
-          </Link>
-          <Link
-            className="flex items-center justify-center rounded-full border bg-yellow-300"
+            Google 게정으로 로그인
+          </OAuthLink>
+          <OAuthLink
+            alt="카카오 로그인"
+            icon={KakaoIcon}
             href={`https://kauth.kakao.com/oauth/authorize?${kakao}`}
           >
-            <Image
-              width={42}
-              height={42}
-              src={KakaoIcon}
-              alt="login to kakao"
-            />
-          </Link>
+            카카오 게정으로 로그인
+          </OAuthLink>
         </div>
       </div>
     </Container>
