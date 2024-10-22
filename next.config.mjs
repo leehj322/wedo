ad466d554fb0fd3cf4db2a237df0df1b6dd59b0b
gjrefa9139 <gjrefa9139@gmail.com>
2024-10-22 16:34:41 +0900

    Design/Style: address 태그 기본 스타일 변경

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
         protocol: "https",
         hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
         port: "",
+        pathname: "/**",
       },
     ],
   },
