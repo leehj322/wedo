06cd9e6182e6668e5848be96d84f8795877bf69b
hvrain <hvrain3222@naver.com>
2024-10-23 01:37:01 +0900

    Fix: 카카오, 구글 프로필 이미지를 위한 next.config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,16 @@ const nextConfig = {
         hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
         port: "",
       },
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+				port: "",
+			},
+			{
+				protocol: "http",
+				hostname: "img1.kakaocdn.net",
+				port: "",
+			}
     ],
   },
   webpack: (config) => {
