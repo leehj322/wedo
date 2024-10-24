8fd4d53de72dd47b78ba5a317d9356d70e479281
leehj322 <>
2024-10-24 09:12:07 +0000

    Fix: routes.ts 파일 업로드

--- routes.ts
+++ routes.ts
@@ -2,6 +2,8 @@ export const publicRoutes = ["/"];
 
 export const authRoutes = ["/login", "/signup"];
 
+export const resetPasswordRoutes = "/reset-password";
+
 export const apiAuthPrefix = "/api/auth";
 
 export const DEFAULT_LOGIN_REDIRECT = "/";
