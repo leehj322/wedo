691f8afc31f670095d8de175cfa0bf8f4106cef6
leehj322 <>
2024-11-04 05:20:56 +0000

    Feat: error.tsx 추가 #167

---
+++ app/(pages)/error.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <main className="fixed left-1/2 top-1/2 flex h-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
+      <h2 className="text-center">{error.message}</h2>
+      <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        onClick={
+          // Attempt to recover by trying to re-render the invoices route
+          () => reset()
+        }
+      >
+        다시시도
+      </button>
+    </main>
+  );
+}
